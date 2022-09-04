8dff07ad4ad5ae940d2569ae625a4cdadd3b03ce
trustpizza <maxelolsson@gmail.com>
2022-09-04 14:30:54 -0500

    pseudocode

---
+++ script.rb
@@ -0,0 +1,9 @@
+def bubblesort(array)
+    for k from 1 to n-1
+        for i from 0 to n-2
+            if array[i] > array[i+1]
+                swamp array[i] with array[i+1]
+            end
+        end
+    end
+end
